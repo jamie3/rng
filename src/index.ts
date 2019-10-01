8499c75d99eb5cd02836f6e8202c78249795bc46
Jamie <jamie.archibald@neofinancial.com>
2019-09-30 20:39:24 -0600

    Removed logging

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 import bodyParser from "body-parser";
 import express, { Request, Response } from "express";
-import { Random, MersenneTwister19937 } from "random-js";
+import { MersenneTwister19937, Random } from "random-js";
 
 const random = new Random(MersenneTwister19937.autoSeed());
 const server = express();
@@ -10,8 +10,6 @@ server.use(bodyParser.urlencoded({ extended: true }));
 const port = 3000;
 
 server.post("/", (req: Request, res: Response) => {
-  console.log(req.body);
-
   const result = random.integer(Number(req.body.start), Number(req.body.end));
 
   res.json(result);
