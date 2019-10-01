c2936895a1e252649929a3dec5582e19a26ae2d1
Jamie <jamie.archibald@neofinancial.com>
2019-09-30 20:35:46 -0600

    Added seed

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,8 @@
 import bodyParser from "body-parser";
 import express, { Request, Response } from "express";
-import { Random } from "random-js";
+import { Random, MersenneTwister19937 } from "random-js";
 
-const random = new Random();
+const random = new Random(MersenneTwister19937.autoSeed());
 const server = express();
 server.use(bodyParser.json());
 server.use(bodyParser.urlencoded({ extended: true }));
